bf5b843a3dea3385fd8e552ef11eabf9552ca761
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2025-07-30 05:43:11 +1000

    feat: init

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs@{ nixpkgs, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = nixpkgs.lib.systems.flakeExposed;
+      perSystem =
+        {
+          lib,
+          pkgs,
+          system,
+          config,
+          ...
+        }:
+        {
+          _module.args.pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              (import inputs.rust-overlay)
+            ];
+          };
+
+          devShells.default =
+            with pkgs;
+            let
+              toolchain = pkgs.rust-bin.stable.latest.default.override {
+                extensions = [ "rust-src" ];
+                targets = [ "x86_64-unknown-linux-gnu" ];
+              };
+            in
+            mkShell {
+              nativeBuildInputs =
+                with pkgs;
+                [
+                  toolchain
+                  bun
+                ]
+                ++ lib.optionals stdenv.isLinux [ openssl ]
+                ++ lib.optionals stdenv.isDarwin [ apple-sdk ];
+            };
+        };
+    };
+}
---
+++ src-tauri/src/main.rs
@@ -0,0 +1,39 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use tauri_plugin_updater::UpdaterExt;
+
+fn main() {
+    tauri::Builder::default()
+        .setup(|app| {
+            let handle = app.handle().clone();
+            tauri::async_runtime::spawn(async move {
+                update(handle).await.unwrap();
+            });
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
+async fn update(app: tauri::AppHandle) -> tauri_plugin_updater::Result<()> {
+    if let Some(update) = app.updater()?.check().await? {
+        let mut downloaded = 0;
+        update
+            .download_and_install(
+                |chunk_length, content_length| {
+                    downloaded += chunk_length;
+                    println!("downloaded {downloaded} from {content_length:?}");
+                },
+                || {
+                    println!("download finished");
+                },
+            )
+            .await?;
+
+        println!("update installed");
+        app.restart();
+    }
+
+    Ok(())
+}
